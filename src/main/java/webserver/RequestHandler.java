394ba12ad38afac592968a44e6b8eee98bb7ad5b
sixthou <justyuk@naver.com>
2022-10-16 15:34:36 +0900

    feat : 요구사항 1 - http://localhost:8080/index.html로 접속시 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,24 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
+    public static final StringBuffer STRING_BUFFER = new StringBuffer();
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -25,12 +31,53 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+
+            String request = requestReader(bufferedReader);
+            String url = getUrl(request);
+
+            byte[] body = getHtmlFileFromUrlToByte(url);
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private byte[] getHtmlFileFromUrlToByte(String url) throws IOException {
+        byte[] body;
+        try {
+            body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        } catch (IOException e) {
+            body = "hello world".getBytes(StandardCharsets.UTF_8);
+        }
+
+        return body;
+    }
+
+    private String getUrl(String request) {
+        String[] tokens = request.split(" ");
+        return tokens[1].equals("") || tokens[1].equals("/") ? "" : tokens[1];
+    }
+
+    private String requestReader(BufferedReader bufferedReader) throws IOException {
+        STRING_BUFFER.setLength(0);
+        try {
+            String line = bufferedReader.readLine();
+
+            while (!"".equals(line)) {
+                if (line == null) {
+                    break;
+                }
+                STRING_BUFFER.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
+        return STRING_BUFFER.toString();
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
